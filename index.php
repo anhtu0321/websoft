cdcea6a57f3109e519cc60c9c091e4ddad160012
AnhTu <vantu9486@gmail.com>
2020-05-12 16:29:58 +0700

    lan 1

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-day la trang noi bo cong an tinh
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website nội bộ Công an tỉnh Hưng Yên</title>
+</head>
+<link rel="stylesheet" type="text/css" href="style/bootstrap341/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style/mystyle.css">
+<body>
+    
+    <div class="container">
+        <?php include("include/banner.php");?>
+        <?php include("include/menu.php");?>
+        <?php include("include/main.php");?>
+        <?php include("include/footer.php");?>
+    </div>
+    
+    
+    
+</body>
+<script src="style/bootstrap341/js/jquery.js"></script>
+<script src="css/popper.min.js"></script>
+<script type="text/javascript" src="style/bootstrap341/js/bootstrap.min.js"></script>
+</html>
