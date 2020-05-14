843131471d980e006fb6b592f352ae06704860ea
hanh <thehanhvu@gmail.com>
2020-05-14 09:54:39 +0700

    jskdhajkfhjkas

--- include/banner.php
+++ include/banner.php
@@ -1,7 +1,7 @@
 <div class="row">
     
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <img src="img/banner.jpg" class="img-responsive" alt="Image" width="100%">
+        <img src="img/bannercat.jpg" class="img-responsive" alt="Image" width="100%">
     </div>
  
 
