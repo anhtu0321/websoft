bf46b940f88337b423710b13cfeef3711777f08d
AnhTu <vantu9486@gmail.com>
2020-05-14 09:49:18 +0700

    at

--- include/banner.php
+++ include/banner.php
@@ -1,4 +1,8 @@
 <div class="row">
-	<img src="img/banner.jpg" />
+    
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <img src="img/banner.jpg" class="img-responsive" alt="Image" width="100%">
+    </div>
+ 
 
 </div>
--- include/menu.php
+++ include/menu.php
@@ -1,1 +1,28 @@
-Day la menu
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span> Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Hoạt động CAT</a></li>
+                    <li><a href="#">Tin ANTT trong tỉnh</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Phần mềm ứng dụng <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">PM gửi nhận văn bản</a></li>
+                            <li><a href="#">PM Quản lý phương tiện</a></li>
+                            <li><a href="#">PM quản lý tài sản</a></li>
+                            <li><a href="#">PM quản lý công việc</a></li>
+                            <li><a href="#">PM phân loại vụ việc</a></li>
+                            <li><a href="#">PM Văn thư</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+
+    </div>
+</div>
