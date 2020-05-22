2ed47cb5a7463ab36343cf7e672842aabf76a7a7
AnhTu <vantu9486@gmail.com>
2020-05-22 13:40:15 +0700

    a

---
+++ admin/include/menu.php
@@ -0,0 +1,14 @@
+
+<div class="col-sm-2 menu no-padding">
+    
+    <ul class="list-group">
+        <li class="list-group-item"><a href="#">Quản lý tin tức</a></li>
+        <li class="list-group-item"><a href="#">Quản lý Mục tin</a></li>
+        <li class="list-group-item"><a href="#">Quản lý Phần mềm</a></li>
+        <li class="list-group-item"><a href="#">Quản lý Báo cáo ngày</a></li>
+        <li class="list-group-item"><a href="#">Quản lý Phần mềm</a></li>
+        <li class="list-group-item"><a href="#">Trang giới thiệu</a></li>
+        <li class="list-group-item"><a href="#">Quản lý tài khoản</a></li>
+    </ul>
+    
+</div>
