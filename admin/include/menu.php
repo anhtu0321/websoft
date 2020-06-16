a02757e7c46ca55e02d0b8c28cd9a85ea3fe5b9a
AnhTu <vantu9486@gmail.com>
2020-06-16 14:14:13 +0700

    data

--- admin/include/menu.php
+++ admin/include/menu.php
@@ -3,10 +3,12 @@
     <ul class="list-group"> 
     <?php 
         foreach ($mangchucnang as $key => $value){
-            if($value["trangthai"]==1){
+            $tbxem = mysql_query("select xem from phanquyen where user = '$_SESSION[user_huye_id]' and form = '$value[id]'");
+            $rsxem = mysql_fetch_array($tbxem);
+            if($value["trangthai"]==1 and $rsxem["xem"] == 1){
         ?>
             <a class="list-group-item list-group-item-info" href="index.php?form=<?php echo $value["id"]?>"><?php echo $value["tenmenu"]?></a>
-        <?
+        <?php
             }
         }
     ?>
