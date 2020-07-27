936824967f164ef4700e2eaadc71e3455d81b970
AnhTu <vantu9486@gmail.com>
2020-07-27 15:32:30 +0700

    tu

--- include/chitiettin.php
+++ include/chitiettin.php
@@ -7,28 +7,17 @@ $rs = mysql_fetch_array($tb);
 <div class = "col-sm-12 chi-tiet-tin">
     <!-- tên mục tin -->
     <div class="col-sm-12 no-padding">
-<<<<<<< HEAD
-        <p class="tdmuctin"><a href="index.php?view=muctin&id=<?php echo $rs["id"];?>"><?php echo $rs["tenmuctin"];?></a></p>
-=======
-        <p class="muctin">
-            <a href="index.php?view=muctin&id=<?php echo $rs["id"];?>">
-                <span class="glyphicon glyphicon-th"></span>
-                <?php echo $rs["tenmuctin"];?>
-            </a>
-        </p>
->>>>>>> e51f0005b653f7df31b34f0073ef0523c6204fd2
+        <p class="tdmuctin"><a href="index.php?view=muctin&id=<?php echo $rs["id"];?>"><span class="glyphicon glyphicon-th"></span> <?php echo $rs["tenmuctin"];?></a></p>
+        
     </div>  
     <!-- Tiêu đề tin -->
     <div class="col-sm-12 no-padding">
         <p class="tieude"><?php echo $rs["tieude"];?></p>
         <p class="ngaynhap">Đăng ngày: <?php echo $rs["ngaynhap"];?></p>
     </div>
-<<<<<<< HEAD
-    <div class="col-sm-12">
-=======
     <!-- nội dung tin -->
     <div class="col-sm-12 no-padding">
->>>>>>> e51f0005b653f7df31b34f0073ef0523c6204fd2
+
         <?php echo $rs["noidung"];?>
         <hr/>
     </div>
