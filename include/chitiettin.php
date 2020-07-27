e51f0005b653f7df31b34f0073ef0523c6204fd2
AnhTu <vantu9486@gmail.com>
2020-07-27 15:09:15 +0700

    hoang van tu

--- include/chitiettin.php
+++ include/chitiettin.php
@@ -1,18 +1,35 @@
 <?php
 $id = $_GET["id"];
-$sql = "select tintuc.tieude, muctin.tenmuctin, muctin.id, tintuc.noidung, tintuc.anh from tintuc inner join muctin on tintuc.muctin = muctin.id where tintuc.id='$id'";
+$sql = "select tintuc.tieude, muctin.tenmuctin, muctin.id, tintuc.noidung, tintuc.anh, tintuc.ngaynhap from tintuc inner join muctin on tintuc.muctin = muctin.id where tintuc.id='$id'";
 $tb = mysql_query($sql);
 $rs = mysql_fetch_array($tb);
 ?>
 <div class = "col-sm-12 chi-tiet-tin">
+    <!-- tên mục tin -->
     <div class="col-sm-12 no-padding">
-        <p class="muctin"><a href="index.php?view=muctin&id=<?php echo $rs["id"];?>"><?php echo $rs["tenmuctin"];?></a></p>
+        <p class="muctin">
+            <a href="index.php?view=muctin&id=<?php echo $rs["id"];?>">
+                <span class="glyphicon glyphicon-th"></span>
+                <?php echo $rs["tenmuctin"];?>
+            </a>
+        </p>
     </div>  
+    <!-- Tiêu đề tin -->
     <div class="col-sm-12 no-padding">
         <p class="tieude"><?php echo $rs["tieude"];?></p>
+        <p class="ngaynhap">Đăng ngày: <?php echo $rs["ngaynhap"];?></p>
     </div>
+    <!-- nội dung tin -->
     <div class="col-sm-12 no-padding">
         <?php echo $rs["noidung"];?>
+        <hr/>
     </div>
+    
+    <!-- Tin cùng chuyên mục -->
+    <div class="col-sm-12 no-padding">
+        <p style="font-weight:bold;">
+                Tin cùng chuyên mục
+        </p>
+    </div>  
 </div>
 
