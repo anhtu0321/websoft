bfc03ee1b543131a077c6f718ba18fc43566adb8
hanh <thehanhvu@gmail.com>
2020-09-03 15:16:34 +0700

    alooooooooo

--- include/main/right/logined.php
+++ include/main/right/logined.php
@@ -2,16 +2,16 @@
     <div class="form-group form-group-sm margin-bottom-5">
         <div class="col-sm-12 margin-bottom-5 margin-left-5">
             <?php
-                echo "Welcome ".$_SESSION["user_huye_full_name"]."<br>";
+                echo "<span style = 'font-weight: normal'>Welcome</span> <span class='user'>".$_SESSION["user_huye_full_name"]."</span><br>";
             ?>
         </div>
-        <div class="col-sm-12 margin-bottom-5 margin-left-5">
+        <div class="col-sm-12 margin-bottom-5 margin-left-5 no-bold">
             <a href="index.php?view=bcn">Xem báo cáo ngày</a>
         </div>
-        <div class="col-sm-12 margin-bottom-5 margin-left-5">
+        <div class="col-sm-12 margin-bottom-5 margin-left-5 no-bold">
             <a href="#">Đổi mật khẩu</a>
         </div>
-        <div class="col-sm-12 margin-bottom-5 margin-left-5">    
+        <div class="col-sm-12 margin-bottom-5 margin-left-5 no-bold">    
             <a href="thoat.php">Thoát</a>
         </div>
     </div>
