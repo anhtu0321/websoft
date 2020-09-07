08e3754fa2ba67491f5ee3987b235d8f1d19dbe4
hanh <thehanhvu@gmail.com>
2020-09-07 07:54:38 +0700

    sdff

--- include/main/right/logined.php
+++ include/main/right/logined.php
@@ -8,11 +8,42 @@
         <div class="col-sm-12 margin-bottom-5 margin-left-5 no-bold">
             <a href="index.php?view=bcn">Xem báo cáo ngày</a>
         </div>
-        <div class="col-sm-12 margin-bottom-5 margin-left-5 no-bold">
-            <a href="#">Đổi mật khẩu</a>
-        </div>
+        
         <div class="col-sm-12 margin-bottom-5 margin-left-5 no-bold">    
             <a href="thoat.php">Thoát</a>
         </div>
+        
+        <a class="btn btn-primary" data-toggle="modal" href='#modal-id'>Đổi mật khẩu</a>
+        <div class="modal fade" id="modal-id">
+            <div class="modal-dialog modal-sm">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title">Đổi mật khẩu</h4>
+                    </div>
+                    <div class="modal-body">    
+                        <form action="xldmk.php" method="POST" role="form" enctyp="multipart/form-data">
+                            <div class="form-group">
+                                <label for=""> Mật khẩu cũ</label>
+                                <input type="text" class="form-control" name="oldpass">
+                            </div>
+                            <div class="form-group">
+                                <label for=""> Mật khẩu mới</label>
+                                <input type="text" class="form-control" name="newpass">
+                            </div>
+                            <div class="form-group margin-left-5">
+                                <label for=""> Nhập lại mật khẩu mới</label>
+                                <input type="text" class="form-control" name="renewpass">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Cập nhật</button>
+                        </form>
+                        
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
     </div>
 </form>
---
+++ include/xldmk.php
@@ -0,0 +1,4 @@
+<?php
+    echo "ặklfhasdjfknerg";
+?>
+ádfdf
