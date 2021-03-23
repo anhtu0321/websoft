9477533062b85b8c8d285f10fa3c62685171c88e
Hoang Van Tu <vantu9486@gmail.com>
2021-03-23 16:15:39 +0700

    hoang van tu

--- include/main/center-left.php
+++ include/main/center-left.php
@@ -4,44 +4,44 @@
 	$rsnoibat = mysqli_fetch_array($tbnoibat);
 ?>
 <div class="col-sm-9 module-center">
-			<div class="layout-top">
-				<div class="col-sm-7 box-new">
-					<div class="header-new">
-						<h4><a href="index.php?view=chitiet&id=<?php echo $rsnoibat['id'];?>"><?php echo $rsnoibat["tieude"]; ?></a></h4>
-						<img src="imguploads/<?php echo $rsnoibat['anh']?>"/>
-					</div>
-					<div class="info">
-						<p>
-							<?php 
-                                $tomtat = substr($rsnoibat["noidung"], 0, 350);
-                                $arr_noidung = explode(' ', $tomtat);
-                                array_pop($arr_noidung);
-                                $intomtat = "";
-                                foreach($arr_noidung as $value){
-                                    $intomtat = $intomtat." ".$value;
-                                }
-                                $intomtat = $intomtat."...";
-                                echo $intomtat;
-                            ?>
-						</p>
-					</div>
-				</div>
-				<div class="col-sm-5 list-new">
-					<ul>
-						<?php
-							while($rsnoibat = mysqli_fetch_array($tbnoibat)){
-						?>
-								<li>
-									<a href="index.php?view=chitiet&id=<?php echo $rsnoibat['id'];?>">
-										<img src="imguploads/<?php echo $rsnoibat['anh']?>">
-										<span><?php echo $rsnoibat["tieude"]; ?></span>
-									</a>
-								</li>
-						<?php
-							}
-						?>
-						
-					</ul>
-				</div>
+	<div class="layout-top">
+		<div class="col-sm-7 box-new">
+			<div class="header-new">
+				<h4><a href="index.php?view=chitiet&id=<?php echo $rsnoibat['id'];?>"><?php echo $rsnoibat["tieude"]; ?></a></h4>
+				<img src="imguploads/<?php echo $rsnoibat['anh']?>"/>
+			</div>
+			<div class="info">
+				<p>
+					<?php 
+						$tomtat = substr($rsnoibat["noidung"], 0, 350);
+						$arr_noidung = explode(' ', $tomtat);
+						array_pop($arr_noidung);
+						$intomtat = "";
+						foreach($arr_noidung as $value){
+							$intomtat = $intomtat." ".$value;
+						}
+						$intomtat = $intomtat."...";
+						echo $intomtat;
+					?>
+				</p>
 			</div>
 		</div>
+		<div class="col-sm-5 list-new">
+			<ul>
+				<?php
+					while($rsnoibat = mysqli_fetch_array($tbnoibat)){
+				?>
+						<li>
+							<a href="index.php?view=chitiet&id=<?php echo $rsnoibat['id'];?>">
+								<img src="imguploads/<?php echo $rsnoibat['anh']?>">
+								<span><?php echo $rsnoibat["tieude"]; ?></span>
+							</a>
+						</li>
+				<?php
+					}
+				?>
+				
+			</ul>
+		</div>
+	</div>
+</div>
--- include/main/center-right.php
+++ include/main/center-right.php
@@ -3,20 +3,20 @@
 	$tbtinmoi = mysqli_query($con,$sqltinmoi);
 ?>
 <div class="col-sm-3 module-center">  
-            <div class="border tin-moi">
-                <div class="title">
-                    <a>Tin mới nhất</a>
-                </div>
-                <div class="tin-moi-body">
-                    <ul>
-                        <?php
-                            while($rstinmoi = mysqli_fetch_array($tbtinmoi)){
-                        ?>
-                                <li><a href="index.php?view=chitiet&id=<?php echo $rstinmoi['id'];?>"><?php echo $rstinmoi["tieude"];?></a><br></li>
-                        <?php
-                            }
-                        ?>
-                    </ul>
-                </div>
-            </div>
-		</div>
+    <div class="border tin-moi">
+        <div class="title">
+            <a>Tin mới nhất</a>
+        </div>
+        <div class="tin-moi-body">
+            <ul>
+                <?php
+                    while($rstinmoi = mysqli_fetch_array($tbtinmoi)){
+                ?>
+                        <li><a href="index.php?view=chitiet&id=<?php echo $rstinmoi['id'];?>"><?php echo $rstinmoi["tieude"];?></a><br></li>
+                <?php
+                    }
+                ?>
+            </ul>
+        </div>
+    </div>
+</div>
