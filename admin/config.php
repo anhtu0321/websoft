e01d735f9de4f8363f343b57583c5b9dd848a309
anhtu0321 <vantu9486@gmail.com>
2021-03-27 10:40:21 +0700

    hoang van tu

--- admin/config.php
+++ admin/config.php
@@ -5,5 +5,7 @@ $user = "root";
 $pass = "tuanninh1";
 $database = "websoft";
 $con = mysqli_connect($server,$user,$pass);
+mysqli_set_charset($con, 'latin1');
+// mysqli_set_charset($con, 'utf8');
 mysqli_select_db($con,$database);
 ?>
